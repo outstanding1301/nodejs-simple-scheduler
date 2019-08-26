38073216c409b1030f67a7210713630cd2b4d835
송인걸 <outstandingboy@song-ingeol-ui-MacBookPro.local>
2019-08-27 04:00:31 +0900

    reload, add, del, check

---
+++ app.js
@@ -0,0 +1,186 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express()
+
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(__dirname+'/public'));
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const initdb = {
+  "schedules": [
+    {
+      "done": false,
+      "what": "할 일을 입력하고 등록 버튼을 클릭하세요!"
+    },
+    {
+      "done": true,
+      "what": "등록된 일정을 클릭하면 체크(완료)됩니다!"
+    },
+    {
+      "done": false,
+      "what": "일정을 지우려면 삭제 버튼을 클릭하세요!"
+    },
+  ]
+}
+
+fs.exists('./db', (exists)=>{
+  if(!exists){
+    console.log("[!] 일정이 존재하지 않습니다. 일정을 생성합니다.")
+    fs.writeFile('./db', JSON.stringify(initdb), (err)=>{});
+  }else{
+    console.log("[!] 일정이 존재합니다. 불러옵니다.")
+  }
+})
+
+function reload(){
+  let data = fs.readFileSync('./db')
+    const json = JSON.parse(data);
+    const schedules = json.schedules;
+    if(!schedules){
+      return '';
+    }else{
+      let str = "";
+      for(let id in schedules){
+        const schedule = schedules[id];
+        if(schedule.done){
+          str+='<div class="todo_done">\
+            <div id="todo_item_'+id+'" class="todo_done_box_container" onclick="undone('+id+')">\
+             <div class="check_done">✓</div>\
+              <p class="todo_what_done">'+schedule.what+'</p>\
+            </div>\
+            <div class="todo_done_btn_container">\
+              <div id="btn_del_'+id+'" class="todo_done_btn_delete" onclick="del('+id+')">삭제</div>\
+            </div>\
+          </div>'
+        }
+        else{
+          str+='<div class="todo">\
+            <div id="todo_item_'+id+'" class="todo_box_container" onclick="done('+id+')">\
+             <div class="check">✓</div>\
+              <p class="todo_what">'+schedule.what+'</p>\
+            </div>\
+            <div class="todo_btn_container">\
+              <div class="todo_btn_edit">수정</div>\
+              <div id="btn_del_'+id+'" class="todo_btn_delete" onclick="del('+id+')">삭제</div>\
+            </div>\
+          </div>'
+        }
+      }
+      return str;
+    }
+}
+
+app.get('/', (req, res)=>{
+  const date = new Date();
+  const strfm = ""+date.getFullYear()+"년 "+(date.getMonth()+1)+"월 "+date.getDate()+"일";
+  res.render('index', {date:strfm})
+})
+
+app.post('/add', (req, res)=>{
+  const body = req.body;
+  fs.readFile('./db', (err, data)=>{
+    const json = JSON.parse(data);
+    const schedules = json.schedules;
+    for(let schedule of schedules){
+      if(schedule.what == body.what){
+        res.send(JSON.stringify({"result":"faled", "reason":"이미 등록된 일정"}))
+        return;
+      }
+    }
+    schedules.push(body);
+    fs.writeFile('./db', JSON.stringify({"schedules":schedules}), (err)=>{
+      if(err){
+          res.send(JSON.stringify({"result":"faled", "reason":"등록 실패!"}))
+      }else{
+          res.send(JSON.stringify({"result":"success"}))
+      }
+    })
+  })
+})
+
+app.post('/delete', (req, res)=>{
+  const id = req.body.id;
+  fs.readFile('./db', (err, data)=>{
+    const json = JSON.parse(data);
+    const schedules = json.schedules;
+    schedules.splice(id, 1);
+    fs.writeFile('./db', JSON.stringify({"schedules":schedules}), (err)=>{
+      if(err){
+          res.send(JSON.stringify({"result":"faled", "reason":"삭제 실패!"}))
+      }else{
+          res.send(JSON.stringify({"result":"success"}))
+      }
+    })
+  })
+})
+
+
+app.post('/done', (req, res)=>{
+  const id = req.body.id;
+  fs.readFile('./db', (err, data)=>{
+    const json = JSON.parse(data);
+    const schedules = json.schedules;
+    schedules[id].done = true;
+    fs.writeFile('./db', JSON.stringify({"schedules":schedules}), (err)=>{
+      if(err){
+          res.send(JSON.stringify({"result":"faled", "reason":"체크 실패!"}))
+      }else{
+          res.send(JSON.stringify({"result":"success"}))
+      }
+    })
+  })
+})
+app.post('/undone', (req, res)=>{
+  const id = req.body.id;
+  fs.readFile('./db', (err, data)=>{
+    const json = JSON.parse(data);
+    const schedules = json.schedules;
+    schedules[id].done = false;
+    fs.writeFile('./db', JSON.stringify({"schedules":schedules}), (err)=>{
+      if(err){
+          res.send(JSON.stringify({"result":"faled", "reason":"체크해제 실패!"}))
+      }else{
+          res.send(JSON.stringify({"result":"success"}))
+      }
+    })
+  })
+})
+
+app.post('/reload', (req, res)=>{
+  const str = reload();
+  res.send(str);
+})
+
+app.listen(3000, ()=>{
+  console.log("서버가 3000번 포트에서 구동됩니다.\n --> http://localhost:3000");
+})
+
+exports.app = app
+
+/*
+<div class="todo">
+  <div class="todo_box_container">
+   <div class="check">✓</div>
+    <p class="todo_what">오늘 할 일 입니다.</p>
+  </div>
+  <div class="todo_btn_container">
+    <div class="todo_btn_edit">수정</div>
+    <div class="todo_btn_delete">삭제</div>
+  </div>
+</div>
+
+<div class="todo_done">
+  <div class="todo_done_box_container">
+   <div class="check_done">✓</div>
+    <p class="todo_what_done">오늘 한 일 입니다.</p>
+  </div>
+  <div class="todo_done_btn_container">
+    <div class="todo_done_btn_delete">삭제</div>
+  </div>
+</div>
+*/
